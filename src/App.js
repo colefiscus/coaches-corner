bd6ab08d8406a8ca413a558ac745332268239e45
colefiscus <>
2021-05-11 13:37:44 -0600

    Create initial file structure and Header/Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+    <Header />
+    <Footer />
+    </>
   );
 }
 
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header class='header'>
+      <h1>Coach's Corner</h1>
+      <button>
+        Profile
+      </button>
+      <button>
+        Logout
+      </button>
+    </header>
+  )
+}
+
+export default Header;
